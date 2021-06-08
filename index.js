cda7abcfe3c9dc8fe31063da6e7620d62023bcf1
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-08 11:54:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'; 
+//Welcome To Flatbook Declaration
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`; 
+//Excited Welcome Message Section
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(`Welcome to Flatbook,${currentUser}! `);
+//shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
